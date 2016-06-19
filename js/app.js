9ed2eed0a7aab857ae5646f50bba7293e91f42a2
Tony Samperi <tonysamperibg89@gmail.com>
2016-06-19 13:39:04 +0200

    Deleting GHpages files
    Deleting GHpages files

--- js/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("myApp", ["ngTutorial"])
-    .controller('introController', function ($scope) {
-        $scope.angularLoaded = "Yeah, man!";
-
-        $scope.beginTutorial = function () {
-            $scope.activateTutorial = true;
-        }
-    });
